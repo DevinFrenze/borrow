b0254ea22c9ab059a0397569f8126de921d075a1
Devin Frenze <devinfrenze@Devins-MacBook-Pro.local>
2017-01-11 13:44:05 -0800

    simple routing example test with `curl --request {METHOD} localhost:3000`

--- server/app.js
+++ server/app.js
@@ -3,7 +3,19 @@ import express from 'express'
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello New World!')
+  res.send('Got a get request !')
+})
+
+app.put('/', function (req, res) {
+  res.send('Got a put request !')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a post request !')
+})
+
+app.delete('/', function (req, res) {
+  res.send('Got a delete request !')
 })
 
 app.listen(3000, function () {
